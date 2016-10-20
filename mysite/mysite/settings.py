196b46135b091c9c0b529a402160c095e6337099
Igor Yudin <Igor_Yudin@inbox.ru>
2016-10-21 02:13:14 +0300

    PostgreSQL working database

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -73,11 +73,14 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'projects',
+        'USER': 'postgres',
+        'PASSWORD': '111111Aa',
+        'HOST': 'localhost',
+        'POST': '5432',
     }
 }
 
