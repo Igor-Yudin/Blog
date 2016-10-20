77aec5259a21e48936a768552d936c29c7ef0620
Igor Yudin <Igor_Yudin@inbox.ru>
2016-10-20 21:49:58 +0300

    Comments

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from blog.models import Post
+from blog.models import Comment
 
 admin.site.register(Post)
+admin.site.register(Comment)
